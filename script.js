7aad012657b10f5dae73a576a40765894dfc3f28
ROUSHAN VERMA <roushanvermanitt23@gmail.com>
2024-03-31 22:53:15 +0530

    updated

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// update profile btn
+const updt_prof = document.querySelector(".updt-prof");
+
+updt_prof.addEventListener("click", function() {
+  window.location.href = "./schedule.html";
+});
+
+// pass generate btn
+
+const pass_gen = document.querySelector(".pass-gen");
+
+pass_gen.addEventListener("click", function() {
+  window.location.href = "./pass.html";
+});
+
+// display bus operator btn
+
+const disp_bopt = document.querySelector(".disp-bopt");
+
+disp_bopt.addEventListener("click", function() {
+  window.location.href = "./operator.html";
+});
+
+
+// display bus schedule
+
+const disp_bus = document.querySelector(".disp-bus");
+
+disp_bus.addEventListener("click", function() {
+  window.location.href = "./schedule.html";
+});
+
